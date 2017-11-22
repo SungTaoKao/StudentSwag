e8516d408a15dc19179be2885aefe044ef34ea99
SungTaoKao <paulkao94@gmail.com>
2017-11-22 11:48:47 -0800

    initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string School { get; set; }
+    public DateTime StartDate { get; set; }
+}
